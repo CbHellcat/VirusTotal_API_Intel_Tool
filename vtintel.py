996ef51b985cbfeb4cc06785a68676131d401636
CbHellcat <cbatche@gmail.com>
2013-04-23 19:47:18 -0400

    Script Changes:         Changed date structure of folder names.         Changed imports in script for time, datetime, and ordereddict.         Overhaul of report parsing and report structure.         Changed file extension to match the type given in JSON.         Added version number
    Updated readme to reflect script changes
    
    Updated report examples to reflect script changes

--- vtintel.py
+++ vtintel.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python
 # Virus Total API Intelligence Report and Download Script
+# Version = 1.1
 # Built on VT API Script from Chris Clark and VT Downloader script from Emiliano Martinez.
 # Rewirtten / Modified / Personalized: Clay Batchelor ~ GD Fidelis CyberSecurity
 # Use however you want to! 
+#
+#TODO:  Clean up code, indentation and quote unformity
+#       Clean up/change terminal output
+#       Fix some spacing issues in reports
+#       Add function to avoid downloading files/reports that have already been downloaded
+#
 '''
 This script combines the funtions of Chris Clark's VT script and Emiliano Martinez's VT Intelligence Downloader script. It can be used with Python 2.7. 
  
 If you do not have a Private API key that allows access to Intelligence searching, downloading, and reports, then this 
 script will not function properly. 
 '''
-import json, logging, argparse, os, Queue, re, socket, sys, threading, datetime, time, urllib, urllib2, hashlib, codecs
+import json, logging, argparse, os, Queue, re, socket, sys, threading, urllib, urllib2, hashlib, codecs
 from pprint import pprint
+from time import sleep
+from datetime import datetime
+from collections import OrderedDict
+
+__VERSION__ = "1.1"
 
 Current_Downloads = 5
 
@@ -19,10 +31,10 @@ socket.setdefaulttimeout(10)
 LOGGING_LEVEL = logging.INFO
 logging.basicConfig(level=LOGGING_LEVEL,
                     format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d_%H:%M:%S',
+                    datefmt='%b-%d-%y_%H:%M:%S',
                     stream=sys.stdout)
-        
-                
+
+
 class Error(Exception):
   """Base-class for exceptions in this module."""
 
@@ -50,7 +62,7 @@ def md5sum(filename):
 
 
 def downloadFolder(query=None):
-  folder_path = os.path.join(os.getcwd(), 'VT_Files', datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+  folder_path = os.path.join(os.getcwd(), 'VT_Files', datetime.now().strftime('%b-%d-%y_%H:%M:%S'))
   try:
     os.makedirs(folder_path)
   except OSError, e:
@@ -64,7 +76,7 @@ def downloadFolder(query=None):
 
 class vtAPI():
     def __init__(self):
-        self.api = 'Enter API Key'
+        self.api = ''
         self.search = ('https://www.virustotal.com/intelligence/search/'
                            'programmatic/')
         self.download = ('https://www.virustotal.com/intelligence/download/'
@@ -85,7 +97,7 @@ class vtAPI():
           break
         except Exception:
           attempts += 1
-          time.sleep(1)
+          sleep(1)
       if not response:
         return (None, None)        
         
@@ -153,51 +165,99 @@ class vtAPI():
         result = urllib2.urlopen(url,data)
         logging.info("\n\tVirus Total Rescan Initiated for -- " + sha256 + " (Requery in 10 Mins)")
 
+        
+    def parser(self, dictionary, indent=False):
+        dictionary = OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
+        for k, v in dictionary.iteritems():
+            if isinstance(v, dict):
+                yield '\n\t%s: ' %k
+                for value in self.parser(v, True):
+                    yield value
+            else:
+                if indent:
+                    tab = '\t'
+                else:
+                    tab = ''
+                if isinstance(v, list):
+                    c = 0
+                    if tab == '':
+                        tab = '\n\t'
+                    yield '%s%s:' % (tab, k)                    
+                    for nested in v:
+                        c += 1
+                        if isinstance(nested, unicode):
+                            yield '\t\t%s' % nested           
+                            if c == len(v): yield '\n'        
+                        else:
+                            if isinstance(nested, dict):
+                                for nestdict in self.parser(nested, True):
+                                    yield '\t' + nestdict
+                            else:        
+                                yield '\t%s: ' % ','.join(map(str, nested)) 
+                          
+                else:
+                    yield "%s%s: %s"%(tab,k,v)
+
 
 def report(raw, sha256, verbose, jsondump, reportname):
   if raw["response_code"] == 0:
-    print sha256 + " -- Not Found in VT"
+    logging.info(sha256 + ' -- Not Found in VT')
     return 0
-
-  report = codecs.open(reportname + '_Report.txt', 'w', encoding='utf-8') 
-  regular = ('\n\tResults for MD5: ',raw['md5'], '\n\tResults for SHA1: ',raw['sha1'], '\n\tResults for SHA256: ',raw['sha256'], '\n\n\tDetected by: ',raw['positives'],'/',raw['total'],'\n\tKaspersky Detection:',raw['scans']['Kaspersky']['result'], '\n\tScanned on: ',raw['scan_date'],'\n\tFirst Seen: ',raw['first_seen'],'\n\tLast Seen: ',raw['last_seen'],'\n\tUnique Sources: ',raw['unique_sources'],'\n\tSubmission Names:')
+  vt = vtAPI()
+  output = list()
+  output.append('=================================\n')
+  output.append('VirusTotal Report for %s' % sha256)
+  output.append('\n=================================\n')
+  output.append('MD5: %s' % raw['md5']) 
+  output.append('SHA1: %s' % raw['sha1']) 
+  output.append('SHA256: %s\n' % raw['sha256']) 
+  output.append('Detected by: %s/%s' % (raw['positives'], raw['total']))
+  try:  
+    output.append('Kaspersky Detection: %s' % raw['scans']['Kaspersky']['result'])
+  except KeyError:
+    output.append('Kaspersky Detection: None')  
+  output.append('Scanned On: %s' % raw['scan_date'])
+  output.append('First Seen: %s' % raw['first_seen'])
+  output.append('Last Seen: %s' % raw['last_seen'])
+  output.append('Unique Sources: %s' % raw['unique_sources'])
+  output.append('File Tags: %s' % ', '.join(map(str, raw['tags'])))
+  output.append('File Type: %s' % raw['type'])
+  output.append('File Size: %s' % raw['size'])
+  output.append('Submission Names:')
   for x in raw['submission_names']:
-    regular += ('\n\t\t',x) 
-  for line in regular:
-    try:
-      string = unicode(line, 'utf-8')
-    except TypeError:
-      string = str(line)
-    report.write(string)  
+    output.append('\t%s' % x) 
 
   if jsondump == True:
     jsondumpfile = codecs.open(reportname + '.json', 'w', encoding='utf-8')
     pprint(raw, jsondumpfile)
     jsondumpfile.close()
 
-  if verbose == True: 
-    report.write('\n\n')
-    report.write('\n\tVerbose VirusTotal Information Output:\n')
-    for line in raw['additional_info'].keys():
-      if line == 'behaviour-v1':
-       behavior = codecs.open(reportname + '_Behavior.txt', 'w', encoding='utf-8')
-       breport = json.dumps(raw['additional_info']['behaviour-v1'], sort_keys=True, indent=4, separators=(',', ': '))
-       behavior.write(breport)
-       behavior.close()
-       unfor = open(reportname + '_Behavior.txt').read()
-       formatted = re.sub('[^a-zA-Z0-9\:\\\<\>\n\.]', ' ', unfor)
-       open(reportname + '_Behavior.txt', 'w').write(formatted)
-       behavior.close()
-       del raw['additional_info']['behaviour-v1']
-    report.write(json.dumps(raw, sort_keys=True, indent=4, separators=(',', ': ')))
-    report.close()
-    unfor = open(reportname + '_Report.txt').read()
-    formatted = re.sub('[^a-zA-Z0-9\:\\\<\>\n\.]', ' ', unfor)
-    open(reportname + '_Report.txt', 'w').write(formatted)
-    report.close()
+  if verbose == True:
+      output.append('\n=================================\n')
+      output.append('VirusTotal Verbose Report')
+      output.append('\n=================================\n')
+      output.append('Additional File Information:')
+      output.append('----------------------------\n')
+      for line in raw['additional_info'].keys():
+          if line == 'behaviour-v1':
+              behavior = list()
+              behavior.append('=================================\n')
+              behavior.append('VirusTotal Behavior Report for %s' % sha256)
+              behavior.append('\n=================================\n')
+              for parsed_data in vt.parser(raw['additional_info']['behaviour-v1']):
+                  behavior.append('\t%s' % parsed_data)
+              codecs.open(reportname + '_Behavior.txt', 'w', encoding='utf-8').write('\n'.join(behavior))
+              del raw['additional_info']['behaviour-v1']    
+      for parsed_data in vt.parser(raw['additional_info']):
+          output.append('\t%s' % parsed_data)
+      output.append('\n--------------------')
+      output.append('AV Scan Information:')             
+      output.append('--------------------\n')    
+      for parsed_data in vt.parser(raw['scans']):
+          output.append('\t%s' % parsed_data)       
+  codecs.open(reportname + '_Report.txt', 'w', encoding='utf-8').write('\n'.join(output))
+
     
-
-
 def main():
   opt=argparse.ArgumentParser(
       prog='vtintel.py', 
@@ -249,6 +309,7 @@ Example Commands:
   
   vt=vtAPI()
   
+  logging.info('Virus Total API Intelligence Report and Download Script v%s' % __VERSION__)
   logging.info('Starting VirusTotal Search')
   logging.info('* VirusTotal Intelligence search: %s', options.Query)
   logging.info('* Number of files to retrieve for download/report: %s', options.Number)
@@ -260,18 +321,19 @@ Example Commands:
     while not end_process:
       try:
         sha256, folder = work.get(True, 3)
+        rawreport = vt.getReport(sha256) 
       except Queue.Empty:
         continue
       if options.report or options.jsondump or options.verbose:
         reportname = os.path.join(folder, sha256)
-        report(vt.getReport(sha256), sha256 ,options.verbose, options.jsondump, reportname)
+        report(rawreport, sha256 ,options.verbose, options.jsondump, reportname)
       if options.pcap:
         name = os.path.join(folder, sha256 + '.pcap')
         vt.downloadPcap(sha256,name)
       if options.force:
         vt.rescan(sha256)
       if options.download:
-        destination_file = os.path.join(folder, sha256 + '.danger')
+        destination_file = os.path.join(folder, sha256 + '._' + rawreport['type'] + '_')
         logging.info('Downloading file %s', sha256)
         success = vt.downloadFile(sha256, destination_file)
         if success:
@@ -318,7 +380,7 @@ Example Commands:
       if wait:
         logging.info('Waiting for queued files to finish')
         while work.qsize() > 0:
-          time.sleep(5)
+          sleep(5)
         end_process = True
         for thread in threads:
           if thread.is_alive():
